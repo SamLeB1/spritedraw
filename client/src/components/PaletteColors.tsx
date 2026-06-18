79d01bba4e334472cc43a288e16ed6f8edf9f07d
Sam <leblanc.SAL1@gmail.com>
2026-06-18 08:27:25 -0300

    added indicators to selected palette colors

--- client/src/components/PaletteColors.tsx
+++ client/src/components/PaletteColors.tsx
@@ -6,28 +6,54 @@ type PaletteColorsProps = {
 };
 
 export default function PaletteColors({ colors }: PaletteColorsProps) {
+  const primaryColor = useEditorStore((s) => s.primaryColor);
+  const secondaryColor = useEditorStore((s) => s.secondaryColor);
   const setPrimaryColor = useEditorStore((s) => s.setPrimaryColor);
   const setSecondaryColor = useEditorStore((s) => s.setSecondaryColor);
 
   return (
     <div className="grid max-h-28 grid-cols-8 gap-1 overflow-x-hidden overflow-y-auto">
-      {colors.map((color, i) => (
-        <div
-          key={i}
-          className={`aspect-square cursor-pointer hover:border ${
-            tinycolor(color).isLight()
-              ? "hover:border-black"
-              : "hover:border-white"
-          }`}
-          style={{ backgroundColor: color }}
-          title={color}
-          onClick={() => setPrimaryColor(color)}
-          onContextMenu={(e) => {
-            e.preventDefault();
-            setSecondaryColor(color);
-          }}
-        />
-      ))}
+      {colors.map((color, i) => {
+        const isLight = tinycolor(color).isLight();
+        const indicatorColor = isLight ? "black" : "white";
+        const isPrimary = tinycolor.equals(color, primaryColor);
+        const isSecondary = tinycolor.equals(color, secondaryColor);
+
+        return (
+          <div
+            key={i}
+            className={`relative aspect-square cursor-pointer hover:border ${
+              isLight ? "hover:border-black" : "hover:border-white"
+            }`}
+            style={{ backgroundColor: color }}
+            title={color}
+            onClick={() => setPrimaryColor(color)}
+            onContextMenu={(e) => {
+              e.preventDefault();
+              setSecondaryColor(color);
+            }}
+          >
+            {isPrimary && (
+              <div
+                className="absolute top-0 left-0 h-0 w-0"
+                style={{
+                  borderTop: `0.5rem solid ${indicatorColor}`,
+                  borderRight: "0.5rem solid transparent",
+                }}
+              />
+            )}
+            {isSecondary && (
+              <div
+                className="absolute right-0 bottom-0 h-0 w-0"
+                style={{
+                  borderBottom: `0.5rem solid ${indicatorColor}`,
+                  borderLeft: "0.5rem solid transparent",
+                }}
+              />
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
